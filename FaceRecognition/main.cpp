92cf1274c886c91b9e56add165a1d000dab1a5e6
芒种 <248711554@qq.com>
2019-03-20 18:56:22 +0800

    主窗口布局修改

--- FaceRecognition/main.cpp
+++ FaceRecognition/main.cpp
@@ -5,6 +5,10 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    QFont f("ZYSong18030",12);
+
+    w.setFont(f);
+    w.setWindowTitle("");
     w.show();
 
     return a.exec();
